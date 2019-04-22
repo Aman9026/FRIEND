2ba8d063f8c5c98aae7028aa935af9506d508e8e
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2019-04-22 17:53:57 +0530

    seconds game's if block menu added

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -946,4 +946,128 @@ void play_dice(int & score)
 
     */
 
+	    
+	    
+
+
+
+    //IF BLOCK FOR THE SECOND GAME
+
+
+    if(xx==2)
+    {
+
+     int player = 1,i,choice;
+
+    char mark;
+
+    do
+    {
+
+        board();
+
+
+        player=(player%2)?1:2;
+
+
+        cout << "Player " << player << ", enter a number:  ";
+
+
+        cin >> choice;
+
+
+        mark=(player == 1) ? 'X' : 'O';
+
+
+        if (choice == 1 && square[1] == '1')
+
+            square[1] = mark;
+
+
+        else if (choice == 2 && square[2] == '2')
+
+            square[2] = mark;
+
+
+        else if (choice == 3 && square[3] == '3')
+
+            square[3] = mark;
+
+
+        else if (choice == 4 && square[4] == '4')
+
+            square[4] = mark;
+
+
+        else if (choice == 5 && square[5] == '5')
+
+            square[5] = mark;
+
+
+        else if (choice == 6 && square[6] == '6')
+
+            square[6] = mark;
+
+
+        else if (choice == 7 && square[7] == '7')
+
+            square[7] = mark;
+
+
+        else if (choice == 8 && square[8] == '8')
+
+            square[8] = mark;
+
+
+        else if (choice == 9 && square[9] == '9')
+
+            square[9] = mark;
+
+        else
+        {
+
+            cout<<"Invalid move ";
+
+            player--;
+
+            cin.ignore();
+
+            cin.get();
+        }
+
+        i=checkwin();
+
+
+        player++;
+
+    }while(i==-1);
+
+    board();
+
+    if(i==1)
+
+        cout<<"==>\aPlayer "<<--player<<" win ";
+    else
+        cout<<"==>\aGame draw";
+
+    cin.ignore();
+
+    cin.get();
+
+    return 0;
+}
+
+    if(xx!=1||xx!=2) {cout<<"\n\n\n\t\tYOU CHOOSE INCORRECT OPTION :( TRY AGAIN\n\n\n\n";}
+
+}
+
+if(x!=1||x!=2) {cout<<"\n\n\n\t\tYOU CHOOSE INCORRECT OPTION :( TRY AGAIN\n\n\n\n";}
+}
+
+
+
+
+
+
+
 
